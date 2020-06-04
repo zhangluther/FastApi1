e4f2da3dca411c069db377fdd3a97f2cbc3f0e29
Zhang Wei <zhangluther@yahoo.com>
2020-06-04 21:58:02 +0800

    0.0

---
+++ main1.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/people")
+def get_people(id: int, name: str = None):
+    return {"people_id": id, "people_name": name}
